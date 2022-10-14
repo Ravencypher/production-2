66dcdbbea119243e08a1b3b261c321e91fcc9793
Nathalie-newdev <nath94val@hotmail.fr>
2022-10-13 20:02:08 -0400

    Connection MongoDB

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { connecter } = require("./db/connect");
 const app = express();
 
 //création de point de terminaison
@@ -6,6 +7,15 @@ app.get("/", (req, res) => {
     console.log("Salut les filles!")
 });
 
-//création du serveur
-app.listen(3000);
-console.log("Attente des requêtes au port 3000");
+connecter("mongodb://localhost:27017/", (erreur) => {
+    if(erreur){
+      console.log("Erreur lors de la connexion avec la base de données");
+      process.exit(-1);  
+    }else{
+        console.log("Connexion avec la base de données établie");
+        //création du serveur
+        app.listen(3000);
+        console.log("Attente des requêtes au port 3000");
+    }
+});
+
